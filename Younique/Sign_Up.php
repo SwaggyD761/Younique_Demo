782bc33d6182b44263371f89493e1448c541b85f
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-14 11:31:00 +0200

    Create Sign_Up.php

---
+++ Younique/Sign_Up.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>terra_crypto</title>
+    <link rel="stylesheet" href="css/websiteStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	
+	<div class="content">
+		
+		<div class="register-area">
+			<div class="title">
+			</div>
+			
+			<div class="form-area">
+			
+				<form class="register-form" method="POST" action="">
+					
+					<div class="register-data-entry">
+					
+						<div class="name-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						<div class="surname-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						<div class="email-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						<div class="cell-number-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						<div class="password-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						<div class="confirm-password-entry">
+							<div class="label"></div>
+							<div class="input"></div>
+						</div>
+						
+						<div class="form-submit-button">
+							<div class="submit-button"></div>
+						</div>
+						
+					</div>
+					
+				</form>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</body>
