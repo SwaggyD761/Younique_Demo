42039bc8175a67ad2300180b3183c30407d59c69
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-14 11:33:03 +0200

    Create Home.php

---
+++ Younique/Home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>terra_crypto</title>
+    <link rel="stylesheet" href="css/websiteStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<div class=nav-bar>
+		<?php
+			include("Navbar&Footer/_includeNavbar.php")
+	        ?>
+	</div>
+	
+	<div class="content">
+		<div class="carousel_area">
+			<?php
+				include("Carousel/_includeCarousel.php")
+			?>
+		</div>
+		<div class="service-buble">
+			<div class="nails">
+		        <div>
+			<div class="threading">
+		        <div>
+			<div class="waxing">
+		        <div>
+		<div>
+		
+	</div>
+	
+</body>
+	<?php
+		include("Navbar&Footer/_includeFooter.php")
+	?>
+
+</html>
