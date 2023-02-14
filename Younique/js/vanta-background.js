04652f4f847fd07b6991332f545e61ccd78dd0a3
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-14 19:26:04 +0200

    Create vanta-background.js

---
+++ Younique/js/vanta-background.js
@@ -0,0 +1,14 @@
+VANTA.CLOUDS2({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  backgroundColor: 0xfff3fa,
+  skyColor: 0xffb2ff,
+  cloudColor: 0xa8ceff,
+  speed: 0.90,
+  texturePath: "./gallery/noise.png"
+})
