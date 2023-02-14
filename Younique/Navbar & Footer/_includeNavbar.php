3ad188668702bbb18bad287a02b2f57aff79c100
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-14 11:12:05 +0200

    Create _includeNavbar.php

---
+++ Younique/Navbar & Footer/_includeNavbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>terra_crypto</title>
+    <link rel="stylesheet" href="css/navbarStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+  <div class="nav-bar">
+    <div class="nav-bar-left">
+      <div class="hamburger-menu"></div>
+      <div class="home"></div>
+    </div>
+    
+    <div class="nav-bar-enter">
+      <div class=""></div>
+    </div>
+    
+    <div class="nav-bar-right">
+      <div class="us"></div>
+      <div class="Account"></div>
+    </div>
+    
+  </div>
+
+</html>
