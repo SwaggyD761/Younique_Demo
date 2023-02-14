ab00a163adb3f1d8ac6260923354ceb5d97b267b
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-14 18:00:42 +0200

    Update Bookings.php

--- Younique/Bookings.php
+++ Younique/Bookings.php
@@ -9,49 +9,49 @@
 
 <body>
 	<div class=nav-bar>
-		<?php
-			include("Navbar&Footer/_includeNavbar.php")
+	  <?php
+	    include("Navbar&Footer/_includeNavbar.php")
 	  ?>
 	</div>
 	
 	<div class="content">
-		<div class="appointments">
-			<?php
-				include("Carousel/_includeCarousel.php")
-			?>
-		</div>
+	  <div class="appointments">
+	    <?php
+	      include("Carousel/_includeCarousel.php")
+	    ?>
+	  </div>
     
-		<div class="new-bookings">
-			<div class="create-booking">
-    		<h1>Appointment Booking</h1>
-	    	<form>
-				    <label for="date">Date:</label>
-    				<input type="date" id="date" name="date"><br>
+	  <div class="new-bookings">
+	    <div class="create-booking">
+    	      <h1 class="bookings-form-header">Appointment Booking</h1>
+	      <form class="bookings-form" action="php/bookAppointment.php">
+	        <label class="bookings-form-label" for="date">Date:</label>
+    	        <input class="bookings-form-input" type="date" id="date" name="date"><br>
 
-				    <label for="time">Time:</label>
-				    <input type="time" id="time" name="time"><br>
+	        <label class="bookings-form-label" for="time">Time:</label>
+	        <input class="bookings-form-input" type="time" id="time" name="time"><br>
 
-				    <label for="name">Name:</label>
-  				  <input type="text" id="name" name="name"><br>
+	        <label class="bookings-form-label" for="name">Name:</label>
+  	        <input class="bookings-form-input" type="text" id="name" name="name"><br>
 
-	  	      <label for="surname">Surname:</label>
-  	  	  	<input type="text" id="surname" name="surname"><br>
+	        <label class="bookings-form-label" for="surname">Surname:</label>
+  	        <input class="bookings-form-input" type="text" id="surname" name="surname"><br>
 
- 				    <label for="email">Email:</label>
-			      <input type="email" id="email" name="email"><br>
+ 	        <label class="bookings-form-label" for="email">Email:</label>
+	        <input class="bookings-form-input" type="email" id="email" name="email"><br>
 
- 		   		  <label for="phone">Phone:</label>
- 		 	  	  <input type="tel" id="phone" name="phone"><br>
+ 	        <label class="bookings-form-label" for="phone">Phone:</label>
+ 	        <input class="bookings-form-input" type="tel" id="phone" name="phone"><br>
 
-	 		      <input type="submit" value="Submit">
-		    </form>
+	        <input class="bookings-form-button" type="submit" value="Submit">
+	      </form>
 
-			</div>
+	    </div>
         
-			<div class="blank-area">
-		  <d/iv>
+	    <div class="blank-area">
+	    </div>
 
-		</div>
+	  </div>
 		
 	</div>
 	
