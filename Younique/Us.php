0f2b54f1d80ffa2eb4951cf3654186bc0b3d9ab3
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 14:25:32 +0200

    Update Us.php

--- Younique/Us.php
+++ Younique/Us.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>terra_crypto</title>
+    <link rel="stylesheet" href="css/websiteStyle.css">
+    <script src="js/vanta-background.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
+<body>
+	<div class="contianer">
+	
+	
+			<div class=nav-bar>
+		
+				<?php
+			
+					include("Navbar&Footer/_includeNavbar.php")
+	      ?>
+			</div>
+    
+    	<div class="content">
+
+				<div class="service-buble">
+		    <div>
+				<div class="waxing">
+				<div>
+		
+			</div>
+
+	</div>
+					
+</body>
+			
+	<?php
+		include("Navbar&Footer/_includeFooter.php")
+	?>
+
+</html>
