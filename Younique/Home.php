cef0e00249e20e764014bc6a4208fc092d1779c4
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 14:41:55 +0200

    Update Home.php

--- Younique/Home.php
+++ Younique/Home.php
@@ -3,21 +3,20 @@
 <head>
     <meta charset="utf-8" />
     <title>terra_crypto</title>
-    <link rel="stylesheet" href="css/websiteStyle.css">
+    <link rel="stylesheet" href="css/homeStyle.css">
     <script src="js/vanta-background.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 	<div class="contianer">
+		<div class=nav-bar>
+			<?php
+				include("Navbar&Footer/_includeNavbar.php")
+			?>
+		</div>
 	
 		<div class="content">
-			<div class=nav-bar>
-		
-				<?php
-					include("Navbar&Footer/_includeNavbar.php")
-	        		?>
-			</div>
 			<div class="carousel_area">
 				<?php
 					include("Carousel/_includeCarousel.php")
