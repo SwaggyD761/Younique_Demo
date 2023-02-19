c103ab00a15486007c4be2bd85a775df5a13ae23
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 13:57:06 +0200

    Update Account.php

--- Younique/Account.php
+++ Younique/Account.php
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>terra_crypto</title>
+    <link rel="stylesheet" href="css/accountStyle.css">
+    <script src="js/vanta-background.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
+<body>
+	<div class="contianer">
+	
+		<div class=nav-bar>
+		
+			<?php
+			
+				include("Navbar&Footer/_includeNavbar.php")
+	        	?>
+		</div>
+	
+		<div class="content">
+			<div class="carousel_area">
+				<div class="user-details">
+				
+			  </div>
+        
+        <div class="previous-bookings">
+				
+			  </div>
+        
+			</div>
+			<div class="active-bookings">
+				<div class="active-bookings">
+		    <div>
+				<div class="blank-area">
+			  <div>
+				
+			<div>
+		
+		</div>
+	</div>
+					
+</body>
+					
+	<?php
+		include("Navbar&Footer/_includeFooter.php")
+	?>
+
+</html>
