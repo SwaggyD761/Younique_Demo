57fb728461780248e9cb9c24041d9bc8de2983a5
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 14:11:40 +0200

    Update Login.php

--- Younique/Login.php
+++ Younique/Login.php
@@ -2,45 +2,47 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>terra_crypto</title>
+    <title>Login</title>
     <link rel="stylesheet" href="css/websiteStyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<div class="content">
+	<div class=>
+		<div class="content">
 	
-		<div class="login-area">
+			<div class="login-area">
 		
-			<div class="Login-title">
-				<h2>Login</h2>
-			</div>
+				<div class="Login-title">
+					<h2>Login</h2>
+				</div>
 			
-			<div class="form-area">
-				<form class="login-area" method="POST" action="">
+				<div class="form-area">
+					<form class="login-area" method="POST" action="">
 				
-					<div class="login-data-entry">
+						<div class="login-data-entry">
 					
-						<div class="email-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="password-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="login-and-register-button">
-							<div class="register-button"></div>
-							<div class="login-button"></div>	
+							<div class="email-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="password-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="login-and-register-button">
+								<div class="register-button"></div>
+								<div class="login-button"></div>	
+							</div>
+					
 						</div>
 					
-					</div>
-					
-				</form>
+					</form>
 				
-			</div>
+				</div>
 			
+			</div>
+		
 		</div>
-		
 	</div>
 </body>
 </html>
