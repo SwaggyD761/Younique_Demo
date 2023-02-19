ad5e70d2fc084b8516a6e70aaa043a194cb38697
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 13:45:28 +0200

    Add files via upload

---
+++ Younique/Carousel/_includeCarousel.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="carouselStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+	<div class="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item">
+				<img src="image1.jpg">
+    			</div>
+   			<div class="carousel-item">
+  			    	<img src="image2.jpg">
+ 			</div>
+ 			<div class="carousel-item">
+			    	<img src="image3.jpg">
+			</div>
+  		</div>
+                
+  		<button class="prev-btn">&#8249;</button>
+  		<button class="next-btn">&#8250;</button>
+	</div>
+
+	
+
+
+</html>
