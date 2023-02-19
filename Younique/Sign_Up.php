57ca0bfb8822fefe2a4a7513646ee5bf8b238f37
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-02-19 14:19:40 +0200

    Update Sign_Up.php

--- Younique/Sign_Up.php
+++ Younique/Sign_Up.php
@@ -6,57 +6,60 @@
     <link rel="stylesheet" href="css/websiteStyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+	
 <body>
 	
-	<div class="content">
+	<div class="container">
+		<div class="content">
 		
-		<div class="register-area">
-			<div class="title">
-			</div>
+				<div class="register-area">
+					<div class="title">
+				</div>
 			
-			<div class="form-area">
+				<div class="form-area">
 			
-				<form class="register-form" method="POST" action="">
+					<form class="register-form" method="POST" action="">
 					
-					<div class="register-data-entry">
+						<div class="register-data-entry">
 					
-						<div class="name-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="surname-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="email-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="cell-number-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="password-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
-						<div class="confirm-password-entry">
-							<div class="label"></div>
-							<div class="input"></div>
-						</div>
+							<div class="name-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="surname-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="email-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="cell-number-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="password-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
+							<div class="confirm-password-entry">
+								<div class="label"></div>
+								<div class="input"></div>
+							</div>
 						
-						<div class="form-submit-button">
-							<div class="submit-button"></div>
-						</div>
+							<div class="form-submit-button">
+								<div class="submit-button"></div>
+							</div>
 						
-					</div>
+						</div>
 					
-				</form>
+					</form>
 				
-			</div>
+				</div>
 			
-		</div>
+			</div>
 		
+		</div>
 	</div>
 	
 </body>
